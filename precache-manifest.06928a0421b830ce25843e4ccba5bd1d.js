95d282cdc72a09c48fccc6bb5f9e0c5e10d140d4
Anurag <anurag@Tamannas-MacBook-Pro.local>
2019-12-20 17:49:45 -0800

    Updates

---
+++ precache-manifest.06928a0421b830ce25843e4ccba5bd1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e67e1b5bf7f36bed37",
+    "url": "/diceApp/static/css/main.fb40519c.chunk.css"
+  },
+  {
+    "revision": "69e67e1b5bf7f36bed37",
+    "url": "/diceApp/static/js/main.69e67e1b.chunk.js"
+  },
+  {
+    "revision": "26502817dbe2d450c9cc",
+    "url": "/diceApp/static/js/1.26502817.chunk.js"
+  },
+  {
+    "revision": "7efc781ad7b360ca10b6",
+    "url": "/diceApp/static/js/runtime~main.7efc781a.js"
+  },
+  {
+    "revision": "48034952f249c7f9fc313cf048977e97",
+    "url": "/diceApp/index.html"
+  }
+];
